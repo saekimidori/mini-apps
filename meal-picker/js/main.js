9201181e355ac3433dcaa7f6d38ed362cd908f94
Carol Choi <102566661+saekimidori@users.noreply.github.com>
2022-04-12 15:48:55 -0400

    add files via upload

---
+++ meal-picker/js/main.js
@@ -0,0 +1,27 @@
+
+
+// fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+//     .then(res => res.json()) // parse response as JSON
+//     .then(data => {
+//       console.log(data)
+//       document.getElementById('officeExcuse').innerText = data[0].excuse
+//     })
+//     .catch(err => {
+//         console.log(`error ${err}`)
+//     });
+
+document.getElementById('random').addEventListener('click', randomMeal)
+
+function randomMeal() {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data.meals[0])
+        document.querySelector('h2').innerText = data.meals[0].strMeal
+        document.querySelector('img').src = data.meals[0].strMealThumb
+        document.querySelector('h3').innerText = data.meals[0].strInstructions
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
